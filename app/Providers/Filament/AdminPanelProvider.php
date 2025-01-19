349930a2b696cf0099b0b029878b6cd3bfd5ec1e
anglingkusumandhita <anglingkusumandhita@gmail.com>
2025-01-19 20:08:09 +0700

    background image

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,11 +2,10 @@
 
 namespace App\Providers\Filament;
 
-use Filament\Pages;
 use Filament\Panel;
 use Filament\Widgets;
 use Filament\PanelProvider;
-use Filament\Facades\Filament;
+use Filament\Support\Assets\Css;
 use Filament\Support\Colors\Color;
 use App\Filament\Pages\CustomDashboard;
 use App\Filament\Widgets\StatsOverview;
@@ -18,6 +17,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Support\Facades\FilamentAsset;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 
@@ -25,19 +25,24 @@ class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
+        FilamentAsset::register([
+            Css::make('filament', __DIR__ . '/../../resources/css/filament.css'),
+        ]);
+
         return $panel
             ->default()
             ->id('admin')
             ->path('admin')
-        //    ->login(fn () => view('filament.auth.custom-login'))
+            //    ->login(fn () => view('filament.auth.custom-login'))
             ->login()
+            // ->login(CustomLogin::class)
             ->colors([
                 'primary' => Color::Amber,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
-                // Pages\Dashboard::class,
+                    // Pages\Dashboard::class,
                 CustomDashboard::class,
             ])
             // ->topNavigation()
@@ -45,8 +50,8 @@ class AdminPanelProvider extends PanelProvider
             ->sidebarWidth('15rem')
             ->spa()
             // ->brandName('Filament Demo')
-            // ->brandLogo(asset('images/logo.png'))
-            // ->brandLogoHeight('3rem')
+            // ->brandLogo(asset('images/logo2.png'))
+            ->brandLogoHeight('3rem')
             ->favicon(asset('images/logo.png'))
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
